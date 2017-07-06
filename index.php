1f77bbf696ff828b9aef81d1e242ee2bb1e90c66
Cleber89 <clebersoncorreialima@gmail.com>
2017-07-06 11:14:35 -0300

    Inserir e Alterar

--- index.php
+++ index.php
@@ -5,25 +5,33 @@
 	//Carrega 1 usuário
 	//$root = new Usuario();
 	//$root->loadByid(2);
-
 	//echo $root;
 
 	//Carrega uma lista de usuários
 	//$lista = Usuario::getlist();
-
 	//echo json_encode($lista);
 
 	//Carrega umas lista de usuários buscando pelo login
 	//$search = Usuario::search("j");
-
 	//echo json_encode($search);
 
 	//Carrega um usuários usando login e senha
+	//$usuario = new Usuario();
+	//$usuario->login("cleber", "123456");
+	//echo $usuario;
+
+    //Criando um novo usuário
+	//$aluno = new Usuario("aluno", "!@#$");
+	//$aluno->setDeslogin("Aluno");
+	//$aluno->setDessenha("!@#$");
+	//$aluno->insert();
+	//echo $aluno;
+
+	//Fazendo update em um usuários
 	$usuario = new Usuario();
-	$usuario->login("cleber", "123456");
-
+	$usuario->loadByid(8);
+	$usuario->update("professor", "456789");
 	echo $usuario;
-
 
 
 	/*$sql = new Sql();
